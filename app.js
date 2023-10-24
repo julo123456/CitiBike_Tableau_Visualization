e7a4e9fa8170ce470f5316ad4ae59be843c22a69
Julianne <134167952+julo123456@users.noreply.github.com>
2023-10-24 16:46:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log('Is this working?');
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/app/profile/julianne.lo/vizzes";
+const url = "https://public.tableau.com/views/Story-ObservedIncreaseinCitiBikeRidesPost2020-CovidPandemic/Story-ObservedIncreaseinCitiBikeRidesPost2020-CovidPandemic?:language=en-US&:display_count=n&:origin=viz_share_link";
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
